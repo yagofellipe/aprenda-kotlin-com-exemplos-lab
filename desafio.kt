5b83ca42b3e3e23d02ac5a9185029809cf32ecde
Yago Fellipe <113136498+yagofellipe@users.noreply.github.com>
2023-12-16 16:15:32 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,26 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+class Usuario(val name: String)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class ConteudoEducacional(val nome: String, var duracao: Int = 60)
 
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(
+    val nome: String,
+    var conteudos: List<ConteudoEducacional>,
+    var nivel: String
+) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("${usuario.name} matriculado na formação $nome")
+        inscritos.forEach{println(it.name)}
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    var usu = Usuario("Yago")
+    var algoritmos = ConteudoEducacional("Algoritmo")
+    var formacaoDev = Formacao("Desenvolvedor", listOf(algoritmos), "Intermediário")
+    formacaoDev.matricular(usu)
+    println("${formacaoDev.nivel}")
 }
